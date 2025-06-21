1b2c84a7df53177e9dce71ad4ba7c3cf2fbc7913
prajwal801 <123920656+prajwal801@users.noreply.github.com>
2025-06-21 06:19:22 +0000

    [automatic commit] 2025-06-21 06:19:22 UTC 20.227.140.176

--- part2/app.js
+++ part2/app.js
@@ -5,10 +5,8 @@ require('dotenv').config();
 
 const app = express();
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 app.use(session({
   secret: 'dog-secret-key',
@@ -21,8 +19,11 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const dogRoutes = require('./routes/dogRoutes'); // ✅ Add this
+
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/dogs', dogRoutes);
 
 module.exports = app;
