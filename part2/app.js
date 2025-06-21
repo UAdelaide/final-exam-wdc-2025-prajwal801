192b8ed583984a30711f146dc795a8445c9f3419
prajwal801 <123920656+prajwal801@users.noreply.github.com>
2025-06-21 06:43:26 +0000

    [automatic commit] 2025-06-21 06:43:26 UTC 20.227.140.176

--- part2/app.js
+++ part2/app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('./routes/dog/Routes');
+const dogRoutes = require('./routes/dogs');
 
 
 app.use('/api/walks', walkRoutes);
